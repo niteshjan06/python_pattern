0a8df2621122c30de1b3fa85282eda14442d8eb7
niteshjan06 <niteshjan06@gmail.com>
2025-10-06 21:05:00 +0530

    update pattern_A

--- pattern_A.py
+++ pattern_A.py
@@ -1,1 +1,10 @@
-nothing
+n = int(input("Enter any number: "))
+for i in range(0,n):
+    for j in range(0,n):
+        if(i == 0 or i == n//2):
+            print("A",end=" ")
+        elif(j == 0 or j == n-1):
+            print("A",end=" ")
+        else:
+            print(" ",end=" ")
+    print()
