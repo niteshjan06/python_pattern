41bc9baeba3b1dbcf432a86fd7b13c9f032db59f
niteshjan06 <niteshjan06@gmail.com>
2025-10-07 17:36:47 +0530

    update pattern_W.py

---
+++ pattern_W.py
@@ -0,0 +1,12 @@
+n = int(input("Enter any number: "))
+for i in range(0,n):
+    for j in range(0,n):
+        if(i == j and j >= n//2):
+            print("W",end=" ")
+        elif(i + j == n-1 and i >= n//2):
+            print("W",end=" ")
+        elif(j == 0 or j == n-1):
+            print("W",end=" ")
+        else:
+            print(" ",end=" ")
+    print()
