73e8f1c01b2419ad3e25f97561bf3694ff804e17
niteshjan06 <niteshjan06@gmail.com>
2025-10-08 17:07:12 +0530

    update this

---
+++ pattern_Hollow_half_pyramid.py
@@ -0,0 +1,8 @@
+n = int(input("Enter any number: "))
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        if(j == 1 or i == n or i == j):
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print()
